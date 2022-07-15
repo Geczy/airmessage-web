d2ee062e9fe216a1931463f97d2e5f920b5c1c4e
Matt <Geczy@users.noreply.github.com>
2022-07-15 09:39:24 -0500

    rm service worker

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,14 +35,6 @@ try {
   console.error(error);
 }
 
-// Check that service workers are supported
-if (process.env.VERCEL_ENV === "production" && "serviceWorker" in navigator) {
-  // Use the window load event to keep the page load performant
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js");
-  });
-}
-
 //Loading the Google platform script
 export const promiseGAPI = new Promise<unknown>((resolve) => {
   if (typeof window !== "undefined") {
