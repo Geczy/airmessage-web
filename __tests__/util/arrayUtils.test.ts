c81c57a299e582b7d127ef6c4ea34ccdb4ed202f
Matt <Geczy@users.noreply.github.com>
2022-07-15 10:39:34 -0500

    remove unused pkgs, make jest test use alias
    `npx unimported`

--- __tests__/util/arrayUtils.test.ts
+++ __tests__/util/arrayUtils.test.ts
@@ -1,4 +1,4 @@
-import { arrayContainsAll, groupArray } from "../../lib/util/arrayUtils";
+import { arrayContainsAll, groupArray } from "lib/util/arrayUtils";
 
 type ValueHolder<T> = { value: T };
 type KeyValueHolder<K, V> = { key: K; value: V };
