8c3fd0047464618f1cd6f3b6d4bd119c5f269d3e
Matt <Geczy@users.noreply.github.com>
2022-07-17 12:42:54 -0500

    fix: topbar rerenders

--- components/messaging/master/DetailFrame.tsx
+++ components/messaging/master/DetailFrame.tsx
@@ -4,42 +4,47 @@ import { VideocamOutlined } from "@mui/icons-material";
 
 interface Props {
   title: string;
-  children: React.ReactNode;
+  children?: React.ReactNode;
   className?: string;
 
   showCall?: boolean;
   onClickCall?: () => void;
 }
 
+const TopBar = React.memo(({ title, showCall, onClickCall }: Props) => (
+  <Toolbar>
+    <Typography flexGrow={1} flexShrink={1} flexBasis={0} variant="h6" noWrap>
+      {title}
+    </Typography>
+
+    {showCall && (
+      <IconButton size="large" onClick={onClickCall}>
+        <VideocamOutlined />
+      </IconButton>
+    )}
+  </Toolbar>
+));
+
+TopBar.whyDidYouRender = true;
+
 /**
  * A frame component with a toolbar, used to wrap detail views
  */
-export const DetailFrame = React.forwardRef<HTMLDivElement, Props>(
-  (props, ref) => {
-    return (
-      <Stack height="100%" ref={ref}>
-        <Toolbar>
-          <Typography
-            flexGrow={1}
-            flexShrink={1}
-            flexBasis={0}
-            variant="h6"
-            noWrap
-          >
-            {props.title}
-          </Typography>
+const DetailFrame = React.forwardRef<HTMLDivElement, Props>((props, ref) => {
+  return (
+    <Stack height="100%" ref={ref}>
+      <TopBar
+        title={props.title}
+        showCall={props.showCall}
+        onClickCall={props.onClickCall}
+      />
 
-          {props.showCall && (
-            <IconButton size="large" onClick={props.onClickCall}>
-              <VideocamOutlined />
-            </IconButton>
-          )}
-        </Toolbar>
+      <Divider />
 
-        <Divider />
+      {props.children}
+    </Stack>
+  );
+});
 
-        {props.children}
-      </Stack>
-    );
-  }
-);
+DetailFrame.whyDidYouRender = true;
+export { DetailFrame };
